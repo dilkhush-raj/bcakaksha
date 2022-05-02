f0c3ad55183b0f79e3091268a411778946bd7274
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-05-02 08:47:52 +0530

    a target value check

--- pages/semester2/BCSL021/index.js
+++ pages/semester2/BCSL021/index.js
@@ -9,7 +9,7 @@ function BCSL021() {
     <section className="index">
       <h2>Books</h2>
       <ul className="books">
-        <li><Link href="https://egyankosh.ac.in/bitstream/123456789/11561/1/Section-1.pdf"><a target="_blank">Section-1 C Programming Lab</a></Link></li>
+        <li><Link href="https://egyankosh.ac.in/bitstream/123456789/11561/1/Section-1.pdf"><a target="_parent">Section-1 C Programming Lab</a></Link></li>
       </ul>
     </section>
     </div>
--- pages/semester2/BCSL022/index.js
+++ pages/semester2/BCSL022/index.js
@@ -9,8 +9,8 @@ function BCSL022() {
     <section className="index">
       <h2>Books</h2>
       <ul className="books">
-        <li><Link href="https://www.egyankosh.ac.in/bitstream/123456789/11562/1/Section-1.pdf"><a target="_blank">Section-1 Digital Logic Circuits</a></Link></li>
-        <li><Link href="https://www.egyankosh.ac.in/bitstream/123456789/11563/1/Section-2.pdf"><a target="_blank">Section-2 Assembly Language Programming</a></Link></li>
+        <li><Link href="https://www.egyankosh.ac.in/bitstream/123456789/11562/1/Section-1.pdf"><a target="_self">Section-1 Digital Logic Circuits</a></Link></li>
+        <li><Link href="https://www.egyankosh.ac.in/bitstream/123456789/11563/1/Section-2.pdf"><a target="_top">Section-2 Assembly Language Programming</a></Link></li>
       </ul>
     </section>
     </div>
