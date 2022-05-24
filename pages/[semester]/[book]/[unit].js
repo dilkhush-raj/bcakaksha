f417025838e27bea1c6f032e1b196448768fc191
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-05-24 09:39:14 +0530

    error solved in [unit].js

--- pages/[semester]/[book]/[unit].js
+++ pages/[semester]/[book]/[unit].js
@@ -1,17 +1,12 @@
 import { useRouter } from "next/router";
-import books from "../../../data/pre-ques.json";
-import Link from "next/link";
 import Head from "next/head";
 
-export default function Semester2() {
-  const router = useRouter();
-  const post = books[router.query.book];
-  if (!post) return <p></p>;
+export default function Unit() {
 
   return (
     <div className="index">
       <Head>
-        <title>{post.name + "IGNOU की BCA कक्षा"}</title>
+        <title>IGNOU की BCA कक्षा</title>
         <meta
           name="description"
           content="IGNOU की BCA कक्षा: Ebooks, Syllabus, Previous Year Question Paper, Assignments, Notes and many more."
@@ -22,17 +17,6 @@ export default function Semester2() {
           type="image/x-icon"
         />
       </Head>
-      <h1>{post.name}</h1>
-      {/* <div>{console.log({post})}</div> */}
-      <div className="book-wrap">
-        {post.book.map((value, index) => (
-          <Link key={index} href={value.path}>
-            <a className="books" target="_blank">
-              <h2>{value.name}</h2>
-            </a>
-          </Link>
-        ))}
-      </div>
     </div>
   );
 };
