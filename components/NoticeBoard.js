4d73fcd3f8933135b722ba0f18d6ce15d93dd867
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-08-07 19:28:00 +0530

    semester 3 all book added

--- components/NoticeBoard.js
+++ components/NoticeBoard.js
@@ -3,17 +3,17 @@ import Link from "next/link";
 export default function NoticeBoard() {
   const Notice = [
     {
-      title: "Tentative Date Sheet for conduct of June 2022 Term End Examination",
+      title: "Hall Ticket for June 2022 Term End Examination",
+      path: "http://www.ignou.ac.in/ignou/bulletinboard/news/latest/detail/Hall_Ticket_for_June_2022_Term_End_Examination-854",
+      className: "nav-text",
+    },
+    {
+      title: "Date Sheet for June 2022 Term End Examination",
       path: "http://www.ignou.ac.in/userfiles/datesheet.pdf",
       className: "nav-text",
     },
     {
-      title: "Extension of last date for submission of Assignments for June 2022 Term End Examination",
-      path: "http://www.ignou.ac.in/userfiles/Notification%20-%20Assignment%20Extension.pdf",
-      className: "nav-text",
-    },
-    {
-      title: "Re-registration for July 2022 Session - Last Date: 30th June 2022",
+      title: "The Last Date for Re-Registration for July 2022 Session extended till 12th August, 2022",
       path: "https://onlinerr.ignou.ac.in/",
       className: "nav-text",
     }
