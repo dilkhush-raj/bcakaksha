ec59be25400cfb1f6122bd14765a4cec302a7faa
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-08-25 19:21:33 +0530

    checking pdf viewer

---
+++ components/PdfViewer.jsx
@@ -0,0 +1,26 @@
+import Script from "next/script";
+
+export default function PdfViewer({url}) {
+  return (
+    <div className="pdf-viewer">
+      <Script
+        src="https://documentcloud.adobe.com/view-sdk/viewer.js"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+      <div id="adobe-dc-view"></div>
+
+      {document.addEventListener("adobe_dc_view_sdk.ready", function () {
+        var adobeDCView = new AdobeDC.View({
+          clientId: "d6a5fb0a611a4a6d87a0cc1c116a25f2",
+          divId: "adobe-dc-view",
+        });
+        adobeDCView.previewFile({
+          content: { location: { url: url } },
+          metaData: { fileName: "Bodea Brochure.pdf" },
+        });
+      })}
+    </div>
+  );
+}
--- pages/[semester]/[book]/index.js
+++ pages/[semester]/[book]/index.js
@@ -4,16 +4,22 @@ import examDate from "../../../data/exam-date.json";
 import Link from "next/link";
 import Head from "next/head";
 import Timer from "../../../components/Timer";
-import Script from 'next/script';
+import PdfViewer from "../../../components/PdfViewer";
+import { useState } from "react";
+
 
 export default function Semester2() {
+  
+  const [pdf, setPdf] = useState(0);
   const router = useRouter();
   const post = books[router.query.book];
   var date = examDate[router.query.book];
   if (!date) date = "August 01 2022 14:00:00 UTC+0530";
   if (!post) return <p></p>;
 
-
+  const Viewpdf = () => {
+    setPdf("/pdf/ECO-02.pdf")
+  };
 
   return (
     <div>
@@ -23,52 +29,29 @@ export default function Semester2() {
           name="description"
           content="IGNOU की BCA कक्षा: Ebooks, Syllabus, Previous Year Question Paper, Assignments, Notes and many more."
         />
-        <link
-          rel="shortcut icon"
-          href="/logo.svg"
-          type="image/x-icon"
-        />
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
       </Head>
       <h1 className="page-heading">{post.name}</h1>
       <div className="index">
-      <Timer  date={date.date}/>
-      
-      {/* <Timer  date="12/31/2023 23:59:59"/> */}
-      {/* <div>{console.log({post})}</div> */}
-      <div className="book-wrap">
-        {post.book.map((value, index) => (
-          <Link key={index} href={value.path}>
-            <a className="books" target="_blank">
-              <h2>{value.name}</h2>
-            </a>
-          </Link>
-        ))}
+        <Timer date={date.date} />
+
+        {/* <Timer  date="12/31/2023 23:59:59"/> */}
+        {/* <div>{console.log({post})}</div> */}
+        <div className="book-wrap">
+          {post.book.map((value, index) => (
+            <Link key={index} href={value.path}>
+              <a className="books" target="_blank">
+                <h2>{value.name}</h2>
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-    <Script
-        src="https://documentcloud.adobe.com/view-sdk/viewer.js"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-      <div id="adobe-dc-view"></div>
 
-{document.addEventListener("adobe_dc_view_sdk.ready", function()
-    {
-        var adobeDCView = new AdobeDC.View({clientId: "d6a5fb0a611a4a6d87a0cc1c116a25f2", divId: "adobe-dc-view"});
-        adobeDCView.previewFile(
-       {
-          content:   {location: {url: "/pdf/ECO-01.pdf"}},
-          metaData: {fileName: "Bodea Brochure.pdf"}
-       })
-    })
-}
+      <div onClick={Viewpdf}>View pdf</div>
 
-
-      
-
-    
-
+      <PdfViewer url={pdf}/>
+      {console.log(pdf)}
     </div>
   );
-};
+}
