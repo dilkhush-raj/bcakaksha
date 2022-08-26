e47385600319845567262fa4a26110a2e2158ad8
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-08-26 13:21:34 +0530

    test

--- pages/[semester]/[book]/[pdf].js
+++ pages/[semester]/[book]/[pdf].js
@@ -1,31 +1,34 @@
 import { useRouter } from "next/router";
+import PdfViewer from "../../../components/PdfViewer";
 import books from "../../../data/semester.json";
 import Link from "next/link";
 import Head from "next/head";
 
 export default function Semester2() {
   const router = useRouter();
-  const post = [router.query.pdf];
+  // console.log(books);
+  // console.log(post);
+  const test = router.query.pdf;
+  
+  // const book = test.book;
+  const post = books[router.query.book];
+  if (!post) return <p></p>;
+
+  // console.log(test);
+  
   console.log(post);
+  // console.log(book);
+
+  const unit = post.book[test];
+
+  const pdf = unit.path;
+  // console.log(pdf); 
 
   return (
     <div className="index">
-      <Head>
-        <title>{post.name + " - IGNOU की BCA कक्षा"}</title>
-        <meta
-          name="description"
-          content="IGNOU की BCA कक्षा: Ebooks, Syllabus, Previous Year Question Paper, Assignments, Notes and many more."
-        />
-        <link
-          rel="shortcut icon"
-          href="/logo.svg"
-          type="image/x-icon"
-        />
-      </Head>
 
+<PdfViewer pdf={pdf}/>
       <div>
-{post}
-
       </div>
     </div>
   );
