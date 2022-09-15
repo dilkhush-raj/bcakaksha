4e3d3ad6b6def8aa37bff2cdbc41923941fed05f
Dilkhush Raj <dilkhush_raj@outlook.com>
2022-09-15 13:08:51 +0530

    update: css and login

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,4 +1,5 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider } from "firebase/auth";
 import { app } from "../firebase/config";
 import Image from "next/image";
 
@@ -9,16 +10,41 @@ export default function Login() {
   const { user } = useUserAuth();
   let auth = getAuth();
   const provider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
 
   const handleSubmit = () => {
     signInWithPopup(auth, provider)
       .then((response) => {
-        console.log(response.user);
+        // console.log(response.user);
       })
       .catch((err) => {
         alert(err.message);
       });
   };
+
+  const loginWithGithub = () => {
+    signInWithPopup(auth, githubProvider)
+  .then((result) => {
+    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+    console.log(token);
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
 
   const handleLogout = async () => {
     try {
@@ -33,7 +59,6 @@ export default function Login() {
     }
   }
 
-console.log(user);
   return (
     <>
       <div className="login">
@@ -41,9 +66,13 @@ console.log(user);
           {CheckUser(user) ? (
             <li onClick={handleLogout} className="nav-text"><Image src="/images/icons8-login-rounded-50.svg" width="20px" height="20px" />Log Out</li>
           ) : (
-            <li onClick={handleSubmit} className="nav-text"><Image src="/images/icons8-login-rounded-50.svg" width="20px" height="20px" />Login</li>
+            <>
+            <li onClick={handleSubmit} className="nav-text"><Image src="/images/icons8-login-rounded-50.svg" width="20px" height="20px" />Google</li>
+            <li onClick={loginWithGithub} className="nav-text"><Image src="/images/icons8-login-rounded-50.svg" width="20px" height="20px" />Github</li>
+            </>
           )}
         </div>
+
       </div>
     </>
   );
--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -2,6 +2,16 @@ import { useUserAuth } from "../../firebase/UserAuthContext";
 import { useRouter } from "next/router";
 import Link from "next/dist/client/link";
 
+import { app, database } from "../../firebase/config";
+import {
+  collection,
+  addDoc,
+  getDoc,
+  doc,
+  updateDoc,
+  setDoc,
+} from "firebase/firestore";
+
 export default function User() {
   const { user } = useUserAuth();
   function CheckUser(user) {
@@ -9,6 +19,29 @@ export default function User() {
       return true;
     }
   }
+  const id = user.uid;
+
+  const dbInstance = collection(database, "users");
+
+  const saveNote = async () => {
+    const userRef = doc(database, "users", id);
+    await setDoc(userRef, {
+      semester: 3,
+      cycle: "July",
+    });
+  };
+
+  const getData = async () => {
+    
+    const noteSnapshot = await getDoc(doc(database, 'users', id));
+    if (noteSnapshot.exists()) {
+      console.log(noteSnapshot.data());
+      return noteSnapshot.data();
+    } else {
+      console.log("Note doesn't exist");
+    }
+  };
+
 
   return (
     <>
@@ -23,9 +56,9 @@ export default function User() {
             </li>
             <li>Account</li>
           </ul>
-          <div>
-            {user.email}
-          </div>
+          <div>{user.email}</div>
+          <button onClick={saveNote}>Add Data</button>
+          <button onClick={getData}>Get Data</button>
         </div>
       ) : (
         <>
