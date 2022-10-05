d75c6e36bf996acfb7fd21909e6f73c11f9af33b
Dilkhush Raj <85922349+dilkhush-raj@users.noreply.github.com>
2022-10-05 23:14:05 +0530

    Fix error

--- pages/pages/notice.js
+++ pages/pages/notice.js
@@ -1,53 +1,8 @@
 import Link from "next/dist/client/link";
 import NoticeBoard from "../../components/NoticeBoard";
-
 import Head from "next/head";
 
-
 function Notice() {
-
-  const { user } = useUserAuth();
-  const adminID = "oRhlndZYfgROLUpRb9x9Uhj86623";
-
-  function CheckUser(user) {
-    if (!user) {
-      return false;
-    } else if ( user.uid == adminID) {
-      return true;
-    }
-  }
-
-  const [notice, setNotice] = useState("");
-  const [link, setLink] = useState("");
-  const [notices, setNotices] = useState([]);
-
-  const date = new Date();
-
-
-  const calculate = (e) => {
-    e.preventDefault();
-    const addNotice = async () => {
-    
-      await addDoc(collection(database, 'notice'), {
-        title: notice,
-        link: link,
-        date: date,
-      });
-    };
-    addNotice();
-    setNotice("");
-    setLink("");
-    
-  }
-
-  const colRef = collection(database, 'notice')
-
-  getDocs(colRef)
-  .then((snapshot) => {
-    const notesList = snapshot.docs.map((doc) => doc.data());
-    setNotices(notesList)
-  })
-
 
   return (
     <div className="page">
@@ -68,52 +23,7 @@ function Notice() {
         </li>
         <li>Notice Board</li>
       </ul>
-
-        <div className="notice-board">
-        <h2>Notice Board</h2>
-        <ul>
-          {notices.map((item, index) => {
-            return (
-              <Link key={index} className="nav-text" href={item.link}>
-                <a target="_blank">
-                  <li>{item.title}</li>
-                  <br />
-                </a>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>
-
-          {CheckUser(user) ? (
-            <>
-            <form onSubmit={calculate} className="grade-form">
-          <div>
-            <label>Notice</label>
-            <br />
-            <input
-              value={notice}
-              onChange={(e) => setNotice(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>Link</label>
-            <br />
-            <input
-              value={link}
-              onChange={(e) => setLink(e.target.value)}
-            />
-          </div>
-          <Button type="submit" variant="contained">
-            Add
-          </Button>
-        </form>
-        <br /></>
-          ) : (
-            <></>
-          )}
-
-        
+        <NoticeBoard />
     </div>
   );
 }
